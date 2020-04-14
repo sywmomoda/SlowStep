2360263340cae22208e8efed000238daccf44636
syw <2225316642@qq.com>
2020-04-14 11:02:16 +0800

    - flip words

--- src/study/AboutString/FlipOrderOfWords.java
+++ src/study/AboutString/FlipOrderOfWords.java
@@ -9,10 +9,12 @@ public class FlipOrderOfWords {
     public static void main(String[] args) {
         String s = "hello  world!";
         System.out.println(reverseWords(s));
+        System.out.println(reverseWords2(s));
     }
 
     /**
-     * 分割+倒序
+     * split() 分割+倒序
+     * 时间复杂度：O(n)， 空间复杂度：O(n)
      * @param s
      * @return
      */
@@ -29,5 +31,22 @@ public class FlipOrderOfWords {
         }
         return sb.toString().trim();
 
+    }
+
+    /**
+     * 双指针，倒序遍历字符串，记录单词左右边界索引，添加到 StringBuffer。
+     * 时间复杂度：O(n)， 空间复杂度：O(n)
+     */
+    public static String reverseWords2(String s){
+        s = s.trim();
+        int i = s.length()-1,j = i;
+        StringBuffer sb = new StringBuffer();
+        while(i>=0){
+            while((i>=0) && s.charAt(i) != ' ') i--;
+            sb.append(s.substring(i+1,j+1)+" ");
+            while((i>=0) && s.charAt(i) == ' ') i--;
+            j = i;
+        }
+        return sb.toString().trim();
     }
 }
